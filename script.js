0961e0d7d00f6f30ec1b8e206a466c88ee7ca60a
Nnenna Nwabuisi <nwabuisinne@gmail.com>
2023-04-20 12:12:42 +0100

    Task completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Write your code here
+const lBulb1 = document.querySelector("#lightbulb1");
+const lBulb2 = document.querySelector("lightbulb2");
+const lBulb3 = document.querySelector("lightbulb3");
+const subtitle = document.querySelector(".subtitle")
+let count = 0;
+
+lBulb1.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = count;
+    this.classList.toggle("active")
+})
+
+lBulb2.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = count;
+    this.classList.toggle("active")
+})
+
+lBulb3.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = count;
+    this.classList.toggle("active")
+})
